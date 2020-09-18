ae6d5b193b7c59a23f8918214e9a5bedba8251a8
moha7150 <moha7150@ISS2V0PKH2.scdsb.pri>
2020-09-18 11:25:52 -0400

    ok

--- src/Student.java
+++ src/Student.java
@@ -12,9 +12,43 @@ public class Student {
     always public
     
     */
+    
+    //making more than 1 version of method its called overloading
+    //the ONLY rule with overloading is that the peramiters must be different
+    /*
     public Student(){
         name="";
-        test1=test2=test3=0;
+        test1=0;
+        test2=0;
+        test3=0;
+    }
+    */
+    public Student(String name, int test1,int test2,int test3){
+        this.name = name;
+        this.test1 = test1;
+        this.test2 = test2;
+        this.test3 = test3;
+        //this. referse to the properties. this is done when peramiters are called the same as the peramiters
+    }
+    /*
+    public Student(Student other){
+        name=other.name;
+        test1 = other.test1;
+        test2 = other.test2;
+        test3 = other.test3;
+        
+    }*/
+    // when u have multiple constructor, u can chain them.
+    //chaining constructors -> calling a constructor from within another constructor
+    // saves a bit of time
+    
+    public Student(){
+        this("0",0,0,0);
+        //this reffers to other constructor
+        
+    }
+    public Student(Student other){
+        this(other.name,other.test1,other.test2,other.test3);
     }
     
     public void setName(String n){
@@ -52,4 +86,5 @@ public class Student {
         str += "\n---------------------\nAverage = "+getAverage();
         return str;
     }
+    
 }
--- src/StudentTester.java
+++ src/StudentTester.java
@@ -3,20 +3,41 @@ public class StudentTester {
 
     
     public static void main(String[] args) {
-        Student x = new Student();
+        Student z,zz,y,x = new Student();
+        y = new Student("amy",95,83,80);
+        z= new Student(x);//this is a copy, independant
         x.setName("bob");
         
         x.setScore(1, 70);
         x.setScore(2,85);
         x.setScore(3,49);
         
+        //zz is now a pointer/refrence to x, if u change zz, u change x 
+        zz=x; //because the word new was not used
         System.out.println("my name is "+ x.getName());
         
         for (int i = 1; i <=3; i++) {
             System.out.println("my scores for test "+i+" "+x.getScore(i));
         }
+       
         System.out.println("my average is "+x.getAverage());
+        System.out.println("\n\n");
         System.out.println(x.toString());
+        System.out.println("\n\n");
+        System.out.println(y);
+        System.out.println("\n\n");
+        System.out.println(z);
+        System.out.println("\n\n");
+        System.out.println(zz);
+        System.out.println("\n\n");
+        
+        System.out.println("change x's mark to 50s, using zz as a refrence");
+        zz.setScore(1,50);
+        zz.setScore(2,50);
+        zz.setScore(3,50);
+        for (int i = 1; i <=3; i++) {
+            System.out.println("x's scores for test "+i+" "+x.getScore(i)+" using zz");
+        }
     }
     
 }
